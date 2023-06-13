1684885c3c57c8a7e67c9103811e7f0c51497ad9
morbarz <morbarz@post.bgu.ac.il>
2023-06-13 20:13:27 +0300

    starttttt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module view.atpprojectpartc1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens view.atpprojectpartc1 to javafx.fxml;
+    exports view.atpprojectpartc1;
+}
---
+++ src/main/java/view/atpprojectpartc1/IView.java
@@ -0,0 +1,4 @@
+package view.atpprojectpartc1;
+
+public interface IView {
+}
